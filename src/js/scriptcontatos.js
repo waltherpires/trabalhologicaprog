316e8d18df3c4f610c4c2e1ea5f61081109a6d50
Walther Oliveira Pires <102191627+waltherpires@users.noreply.github.com>
2023-11-25 04:24:25 -0300

    att 7 salvar propostas

--- src/js/scriptcontatos.js
+++ src/js/scriptcontatos.js
@@ -5,35 +5,106 @@ var propostas = [];
 var htmldaProposta = '';
 var qtdpropostas;
 var detalhes;
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------
+document.addEventListener('DOMContentLoaded', function () {
+    var contatosSalvos = localStorage.getItem('contatos');
+    var propostasSalvas = localStorage.getItem('propostas');
+
+    if (contatosSalvos) {
+        contatos = JSON.parse(contatosSalvos);
+        renderContatos();
+    }
+
+    if (propostasSalvas) {
+        propostas = JSON.parse(propostasSalvas);
+        renderPropostas();
+    }
+
+    // Renderizar os contatos
+    function renderContatos() {
+        var lista = document.getElementById("lista");
+
+        contatos.forEach(function (contato) {
+            var contatoNaLista = document.createElement("li");
+            contatoNaLista.setAttribute('id', `contato${contatos.indexOf(contato) + 1}`);
+            var nomeDoContato = document.createTextNode(contato.nome);
+            contatoNaLista.appendChild(nomeDoContato);
+
+            var detalhes = document.createElement('div');
+            detalhes.classList.add("info");
+            detalhes.setAttribute('id', `detalhe${contatos.indexOf(contato) + 1}`);
+            detalhes.style.display = 'none';
+            detalhes.innerHTML = `<p>Nome: ${contato.nome}</p> <p>Endereço: ${contato.endereco}</p> <p>Telefone: ${contato.telefone}</p> <p>E-mail: ${contato.email}</p> <button id="btneditar">Editar Contato</button> <button id="btnexcluir">Excluir Contato</button> <button id="btnproposta">Nova Proposta</button> <button id="btnmostrar">Propostas</button>`;
+
+            lista.appendChild(contatoNaLista);
+            contatoNaLista.parentNode.insertBefore(detalhes, contatoNaLista.nextSibling);
+        });
+    }
+
+    // Renderizar as propostas
+    function renderPropostas() {
+        var detalhes = document.getElementById(`detalhe${contatos.length}`); 
+
+        var textoProposta = document.createElement("table");
+        textoProposta.innerHTML = `<tr> <th>Num</th> <th>Data</th> <th>Valor</th> <th>Status</th></tr>`;
+
+        propostas.forEach(function (proposta) {
+            htmldaProposta += `<tr> <td>${proposta.num}</td> <td>${proposta.data}</td> <td>${proposta.valor}</td> <td>${proposta.status}</td> </tr>`;
+        });
+
+        textoProposta.innerHTML += htmldaProposta;
+        detalhes.appendChild(textoProposta);
+    }
+
+});
+
+   
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 document.addEventListener('click', function(e){
     const el = e.target;
 
-    if(el.classList.contains('criarcontato')){ //Cria novo Cliente
+    //Cria novo Cliente
+    if(el.classList.contains('criarcontato')){ 
         var novoContato = {
             nome: prompt('Digite o nome'),
             endereco: prompt('Digite o endereco'),
             telefone: prompt('Digite o telefone'),
             email: prompt("Digite o email")
         }
-        contatos.push(novoContato);
-        //Usar o localStorage aqui
-        var contatoNaLista = document.createElement("li");  
-        contatoNaLista.setAttribute('id', `contato${contatos.length}`) //Adicionar um id para poder utilizar depois 
-        var nomeDoContato = document.createTextNode(novoContato.nome);
-        contatoNaLista.appendChild(nomeDoContato);
-        
-        detalhes = document.createElement('div');
-        detalhes.classList.add("info");
-        detalhes.setAttribute('id', `detalhe${contatos.length}`);
-        detalhes.style.display = 'none';
-        detalhes.innerHTML = `<p>Nome: ${novoContato.nome}</p> <p>Endereço: ${novoContato.endereco}</p> <p>Telefone: ${novoContato.telefone}</p> <p>E-mail: ${novoContato.email}</p> <button id="btneditar">Editar Contato</button> <button id="btnexcluir">Excluir Contato</button> <button id="btnproposta">Nova Proposta</button> <button id="btnmostrar">Propostas</button>`;
 
-        document.getElementById("lista").appendChild(contatoNaLista); //Adiciona o cliente à lista
-        contatoNaLista.parentNode.insertBefore(detalhes, contatoNaLista.nextSibling); //Adiciona os detalhes após o novo cliente
+        if(novoContato.nome === "" || novoContato.endereco === "" || novoContato.telefone === "" || novoContato.email === ""){
+            alert('Valor invalido. Por favor, insira um valor valido')
+        }
+        else {
+            contatos.push(novoContato);
+
+            //LocalStorage
+            salvarLocalStorage();
+            
+            var contatoNaLista = document.createElement("li");  
+            contatoNaLista.setAttribute('id', `contato${contatos.length}`) //Adicionar um id para poder utilizar depois 
+            var nomeDoContato = document.createTextNode(novoContato.nome);
+            contatoNaLista.appendChild(nomeDoContato);
+            
+            detalhes = document.createElement('div');
+            detalhes.classList.add("info");
+            detalhes.setAttribute('id', `detalhe${contatos.length}`);
+            detalhes.style.display = 'none';
+            detalhes.innerHTML = `<p>Nome: ${novoContato.nome}</p> <p>Endereço: ${novoContato.endereco}</p> <p>Telefone: ${novoContato.telefone}</p> <p>E-mail: ${novoContato.email}</p> <button id="btneditar">Editar Contato</button> <button id="btnexcluir">Excluir Contato</button> <button id="btnproposta">Nova Proposta</button> <button id="btnmostrar">Propostas</button>`;
+
+            document.getElementById("lista").appendChild(contatoNaLista); //Adiciona o cliente à lista
+            contatoNaLista.parentNode.insertBefore(detalhes, contatoNaLista.nextSibling); //Adiciona os detalhes após o novo cliente
+
+            renderContatos();
+        }
+        
 
     }
-
+    //Adicionar Proposta
     if (el.getAttribute('id') === "btnproposta") {
+        detalhes = document.getElementById(`detalhe${contatos.length}`); 
         qtdpropostas = propostas.length;
         var novaProposta = {
             num: propostas.length + 1,
@@ -41,13 +112,22 @@ document.addEventListener('click', function(e){
             valor: prompt("Digite o valor da proposta"),
             status: 'ativa'
         }
-        propostas.push(novaProposta);
-        // LocalStorage aqui
-    
-        var novaLinha = document.createElement("p");
-        novaLinha.innerHTML = `Proposta ${novaProposta.num}: Data - ${novaProposta.data}, Valor - R$${novaProposta.valor}, Status - ${novaProposta.status}`;
+        
+        // LocalStorage
+        if(novaProposta.data === '' || novaProposta.valor === '') {
+            alert('Valor invalido. Por favor, insira um valor valido')
+        } else{
+            propostas.push(novaProposta);
+            salvarLocalStorage();
+            
 
-        detalhes.appendChild(novaLinha);
+            var novaLinha = document.createElement("p");
+            novaLinha.innerHTML = `Num: ${novaProposta.num}; Data: ${novaProposta.data}; Valor: ${novaProposta.valor}; Status: ${novaProposta.status}`
+            detalhes.appendChild(novaLinha);
+
+            renderPropostas();
+        }
+    
     }
     
     
@@ -72,4 +152,13 @@ document.addEventListener('click', function(e){
         }
     }
 
+    //Função do LocalStorage
+    function salvarLocalStorage() {
+        localStorage.setItem('contatos', JSON.stringify(contatos));
+        localStorage.setItem('propostas', JSON.stringify(propostas));
+    }
 })
+
+
+//Verificar se existe algo salvo
+
