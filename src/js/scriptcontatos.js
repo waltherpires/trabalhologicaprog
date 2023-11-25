7d26d710693a6eef1920e8a5e1030f4968eafb94
Walther Oliveira Pires <102191627+waltherpires@users.noreply.github.com>
2023-11-25 00:56:14 -0300

    att6 botao novaproposta

--- src/js/scriptcontatos.js
+++ src/js/scriptcontatos.js
@@ -1,6 +1,10 @@
 
 var contatos = [];
 var flag = 0;
+var propostas = [];
+var htmldaProposta = '';
+var qtdpropostas;
+var detalhes;
 document.addEventListener('click', function(e){
     const el = e.target;
 
@@ -18,18 +22,42 @@ document.addEventListener('click', function(e){
         var nomeDoContato = document.createTextNode(novoContato.nome);
         contatoNaLista.appendChild(nomeDoContato);
         
-        var detalhes = document.createElement('div');
+        detalhes = document.createElement('div');
         detalhes.classList.add("info");
         detalhes.setAttribute('id', `detalhe${contatos.length}`);
         detalhes.style.display = 'none';
-        detalhes.innerHTML = `<p>Nome: ${novoContato.nome}</p> <p>Endereço: ${novoContato.endereco}</p> <p>Telefone: ${novoContato.telefone}</p> <p>E-mail: ${novoContato.email}</p> <button>Editar Contato</button> <button>Excluir Contato</button> <button>Nova Proposta</button> <button>Propostas</button> <button>Propostas</button> <tr> <th>Num</th> <th>Data</th> <th>Valor</th> <th>Status</th> </tr> </table>`;
-    
+        detalhes.innerHTML = `<p>Nome: ${novoContato.nome}</p> <p>Endereço: ${novoContato.endereco}</p> <p>Telefone: ${novoContato.telefone}</p> <p>E-mail: ${novoContato.email}</p> <button id="btneditar">Editar Contato</button> <button id="btnexcluir">Excluir Contato</button> <button id="btnproposta">Nova Proposta</button> <button id="btnmostrar">Propostas</button>`;
+
         document.getElementById("lista").appendChild(contatoNaLista); //Adiciona o cliente à lista
-    
         contatoNaLista.parentNode.insertBefore(detalhes, contatoNaLista.nextSibling); //Adiciona os detalhes após o novo cliente
+
     }
 
-    for(let i = 1; i <= contatos.length; i++) {             //Ligar e Desligar exibição dos detalhes
+    if (el.getAttribute('id') === "btnproposta") {
+        qtdpropostas = propostas.length;
+        var novaProposta = {
+            num: propostas.length + 1,
+            data: prompt("Digite a data de hoje"),
+            valor: prompt("Digite o valor da proposta"),
+            status: 'ativa'
+        }
+        propostas.push(novaProposta);
+        // LocalStorage aqui
+    
+        var novaLinha = document.createElement("p");
+        novaLinha.innerHTML = `Proposta ${novaProposta.num}: Data - ${novaProposta.data}, Valor - R$${novaProposta.valor}, Status - ${novaProposta.status}`;
+
+        detalhes.appendChild(novaLinha);
+    }
+    
+    
+    
+/*     if(el.getAttribute('id') === "btnexcluir")
+    if(el.getAttribute('id') === "btnproposta") */
+
+
+    //Ligar e Desligar exibição dos detalhes
+    for(let i = 1; i <= contatos.length; i++) {             
         let detalheAtual = document.getElementById(`detalhe${i}`)
 
         if(el.getAttribute('id') === `contato${i}`){  
